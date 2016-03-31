0a35111296bffaf7b7c6f7aef1eee5000fae2de6
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2016-03-31 14:40:59 +0200

    nytt design, bruker nå knapper istedenfor lenker

--- radio.py
+++ radio.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 from config import RADIO
 from mpd_conn import dec_vol, inc_vol, init, play, stop
 
-app = Flask("RadioControl")
+app = Flask("RadioControl", static_url_path="/radio/static")
 
 @app.route('/radio/', methods=['GET'])
 def index():
@@ -19,8 +19,22 @@ def index():
             dec_vol()
     
     return render_template("index.html", RADIO=RADIO);
+
+@app.route('/radio/test.html', methods=['GET'])
+def index_test():
+    if request.args.has_key('action'):
+        if request.args['action'] == 'play':
+            play(request.args['channel'])
+        elif request.args['action'] == 'stop':
+            stop()
+        elif request.args['action'] == 'inc_vol':
+            inc_vol()
+        elif request.args['action'] == 'dec_vol':
+            dec_vol()
     
+    return render_template("test.html", RADIO=RADIO);
+
 
 if __name__ == '__main__':
     init()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=True)
